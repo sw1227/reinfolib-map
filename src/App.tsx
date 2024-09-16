37a29f0c660bfdf143ea38fcaae297378009adc6
sw1227 <32032108+sw1227@users.noreply.github.com>
2024-09-16 16:30:56 +0900

    feat: add mapbox map

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
 import axios from 'axios';
+import { useRef, useEffect } from 'react';
 import { useQuery } from '@tanstack/react-query';
+import mapboxgl, { MapOptions } from 'mapbox-gl';
+import MapboxLanguage from '@mapbox/mapbox-gl-language';
 import './App.css'
 
 const fetchStatistics = async () => {
@@ -8,7 +11,28 @@ const fetchStatistics = async () => {
   return data;
 }
 
+const initCenter: [number, number] = [139.4534, 35.4548];
+const initZoom = 9;
+const options: MapOptions = {
+  accessToken: import.meta.env.VITE_MAPBOX_TOKEN,
+  container: 'mapbox-conatainer',
+  style: 'mapbox://styles/mapbox/streets-v11',
+  center: initCenter,
+  zoom: initZoom,
+};
+
 function App() {
+  const map = useRef<mapboxgl.Map | null>(null);
+
+  // Initialize map
+  useEffect(() => {
+    if (map.current) return; // initialize map only once
+    map.current = new mapboxgl.Map(options);
+
+    const language = new MapboxLanguage();
+    if (map) map.current.addControl(language);
+  }, []);
+
   const { data, error, isLoading, refetch } = useQuery({ enabled: false, queryKey: ['statistics'], queryFn: fetchStatistics });
 
   const handleClick = () => {
@@ -24,6 +48,7 @@ function App() {
       <button onClick={handleClick}>Fetch</button>
       {isLoading ? <div>Loading...</div> : <div>Not Loading</div>}
       {data ? <div>{JSON.stringify(data?.status)}</div> : null}
+      <div id="mapbox-conatainer"></div>
     </>
   )
 }
