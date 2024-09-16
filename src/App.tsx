d0a6455869f55397cf7e4cc725cb85234947f8c7
sw1227 <32032108+sw1227@users.noreply.github.com>
2024-09-16 15:26:34 +0900

    feat: add sample fetch button

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,29 @@
+import axios from 'axios';
+import { useQuery } from '@tanstack/react-query';
 import './App.css'
 
+const fetchStatistics = async () => {
+  const url = 'http://localhost:8787/XIT001?year=2024&area=31'; // TODO: local
+  const { data } = await axios.get(url);
+  return data;
+}
+
 function App() {
+  const { data, error, isLoading, refetch } = useQuery({ enabled: false, queryKey: ['statistics'], queryFn: fetchStatistics });
+
+  const handleClick = () => {
+    refetch();
+    console.log('TODO: data:', data);
+    if (error) {
+      console.log('TODO: error:', error);
+    }
+  }
+
   return (
     <>
-      hello
+      <button onClick={handleClick}>Fetch</button>
+      {isLoading ? <div>Loading...</div> : <div>Not Loading</div>}
+      {data ? <div>{JSON.stringify(data?.status)}</div> : null}
     </>
   )
 }
